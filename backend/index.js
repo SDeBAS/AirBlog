ff51483ee66e4e8b3d13e6fa49ee47e39c4db863
Debanjan Basu <62347601+SDeBAS@users.noreply.github.com>
2023-12-06 08:19:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,37 @@
 const express = require("express");
-const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path"); 
+const path = require("path");
 const env = require("dotenv");
-const mongoose = require("mongoose"); 
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 const userRouter = require('./routes/user');
 
-env.config(); 
-app.use(cors(
-    {
-        origin:["https://air-blog-frontend.vercel.app/backend"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+env.config();
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: ["https://air-blog-frontend.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(console.log("connected to mongodb")).catch(err => console.log(err));
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
-app.use("/backend/auth", authRoute)
-app.use("/backend/posts", postRoute)
+app.use("/backend/auth", authRoute);
+app.use("/backend/posts", postRoute);
 app.use('/backend/user', userRouter);
 
 app.use(express.static(path.join(__dirname, "./client/public")));
@@ -42,6 +40,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/public', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend is running");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend is running on port ${PORT}`);
+});
