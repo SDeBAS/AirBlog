5c042b9d61bf7b61a63a09ebe881c1e42ae7600d
Debanjan Basu <62347601+SDeBAS@users.noreply.github.com>
2023-12-06 08:06:07 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://air-blog-backend.vercel.app/backend",
+    baseURL: "https://air-blog-backend.vercel.app",
 });
